2385fba4c5143ae8931c42cbdd8e820cfbf474b2
EggsyOnCode <farzansaqib2003@gmail.com>
2023-10-06 12:55:10 +0500

    prev files purged

--- main.js
+++
@@ -1,7 +0,0 @@
-var States;
-(function (States) {
-    States[States["open"] = 0] = "open";
-    States[States["close"] = 1] = "close";
-    States[States["pending"] = 2] = "pending";
-})(States || (States = {}));
-console.log(States.open);
